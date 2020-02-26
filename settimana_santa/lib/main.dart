9b8a3c11f4aeb84b66ba6b2b8743c9c0dbc61dc1
Dario Castellano <>
2020-02-26 15:27:37 +0100

    splash android

--- settimana_santa/lib/main.dart
+++ settimana_santa/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:settimana_santa/home_route.dart';
 import 'package:settimana_santa/programma_route.dart';
 
 import 'confraternite_route.dart';
@@ -8,13 +9,14 @@ import 'meteo_route.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       //routing info
-      initialRoute: '/',
+      initialRoute: HomeRoute.routeName,
       routes: {
         ConfraterniteRoute.routeName: (context) => ConfraterniteRoute(),
         ProgrammaRoute.routeName: (context) => ProgrammaRoute(),
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
         FotogalleryRoute.routeName: (context) => FotogalleryRoute(),
         MeteoRoute.routeName: (context) => MeteoRoute(),
       },
-      title: 'Settimana Santa 2020',
+      title: 'Passo dopo passo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -33,47 +35,39 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.purple,
+        primaryColor: Colors.red[900],
         fontFamily: 'Morris',
+        canvasColor: Colors.amber[50],
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.red[900],
+            fontFamily: 'Morris',
+            fontSize: 20.0,
+            fontWeight: FontWeight.w500,
+          ),
+          subtitle: TextStyle(
+            color: Colors.red[900],
+            fontFamily: 'Morris',
+            fontSize: 14.0,
+            fontWeight: FontWeight.w500,
+          ),
+          button
+              : TextStyle(
+            color: Colors.red[900],
+            fontFamily: 'Morris',
+            fontSize: 14.0,
+            fontWeight: FontWeight.w500,
+          ),
+          caption
+              :TextStyle(
+            color: Colors.red[900],
+            fontFamily: 'Morris',
+            fontSize: 12.0,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Settimana Santa 2020'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        leading: ImageIcon(AssetImage('assets/images/miserere.png')),
-      ),
+      home: HomeRoute(title: 'Settimana Santa 2020'),
     );
   }
 }
